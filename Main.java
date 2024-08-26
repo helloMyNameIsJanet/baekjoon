2a83a6e392c1554e2616931f4c458650bee51a01
helloMyNameIsJanet <https://github.com/helloMyNameIsJanet/baekjoon.git>
2024-08-26 19:18:07 +0900

    처음

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(bf.readLine());  // 5
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 0; i < cnt; i++) {
+            String numbers = bf.readLine();
+            StringTokenizer st = new StringTokenizer(numbers);
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            //System.out.println(A+B);
+            bw.write(A+B+"\n");
+            bw.flush();
+        }
+        bw.close();
+    }
+}
