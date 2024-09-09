83ddb0d7322f5919a79ff8ba221d1923fae96542
helloMyNameIsJanet <yj081455@gmail.com>
2024-09-09 09:55:07 +0000

    10810번

--- history/Main_10810.java
+++ history/Main_10810.java
@@ -7,8 +7,9 @@ import java.util.Iterator;
 import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
-// last problem solving
-public class Main {
+// history [site] [problem number] [url]
+// history baeckjoon 10810 https://www.acmicpc.net/problem/10810
+public class Main_10810 {
     public static void main(String[] args) throws IOException {
         // 도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 매겨져 있다. 또, 1번부터 N번까지 번호가 적혀있는 공을 매우 많이 가지고 있다. 가장 처음 바구니에는 공이 들어있지 않으며, 바구니에는 공을 1개만 넣을 수 있다.
         // 도현이는 앞으로 M번 공을 넣으려고 한다. 도현이는 한 번 공을 넣을 때, 공을 넣을 바구니 범위를 정하고, 정한 바구니에 모두 같은 번호가 적혀있는 공을 넣는다. 만약, 바구니에 공이 이미 있는 경우에는 들어있는 공을 빼고, 새로 공을 넣는다. 공을 넣을 바구니는 연속되어 있어야 한다.
@@ -37,10 +38,8 @@ public class Main {
         for(int i = 0; i < basket.length; i++) {
             bw.write(basket[i] +" ");
         }
-        
+
         bw.flush();
         bw.close();
     }
 }
-
-
