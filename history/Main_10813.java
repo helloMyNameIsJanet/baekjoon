2a4ef6b086f61ddadd3945862546aee10a013358
helloMyNameIsJanet <yj081455@gmail.com>
2024-09-12 08:26:01 +0000

    풀이

--- history/Main_10813.java
+++ history/Main_10813.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
 // last problem solving
-public class Main {
+public class Main_10813 {
     public static void main(String[] args) throws IOException {
         // 도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 매겨져 있다. 바구니에는 공이 1개씩 들어있고, 처음에는 바구니에 적혀있는 번호와 같은 번호가 적힌 공이 들어있다.
         // 도현이는 앞으로 M번 공을 바꾸려고 한다. 도현이는 공을 바꿀 바구니 2개를 선택하고, 두 바구니에 들어있는 공을 서로 교환한다.
