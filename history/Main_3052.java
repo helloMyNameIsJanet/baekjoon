05c66f615acf0477ef1fe7cbd13ac07695047e8f
helloMyNameIsJanet <yj081455@gmail.com>
2024-09-30 09:07:45 +0000

    나머지

--- history/Main_3052.java
+++ history/Main_3052.java
@@ -8,8 +8,9 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-// last problem solving
-public class Main {
+// history [site] [problem number] [url]
+// history baeckjoon 3052 https://www.acmicpc.net/problem/3052
+public class Main_3052 {
     public static void main(String[] args) throws IOException {
         // 두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
         // 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
